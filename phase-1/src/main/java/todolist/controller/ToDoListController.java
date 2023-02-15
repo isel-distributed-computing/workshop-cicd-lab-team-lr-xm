c0b03ea5013f595a0a2ee84f6162a3112265be9b
Luís Ribeiro <luisribeiro@solvit.pt>
2023-02-15 18:14:09 +0000

    Fix bearer error

--- phase-1/src/main/java/todolist/controller/ToDoListController.java
+++ phase-1/src/main/java/todolist/controller/ToDoListController.java
@@ -64,9 +64,10 @@ public class ToDoListController {
     }
 
     private void validateToken(String authorization) {
-        if (!authorization.startsWith("Bearer: "))
+        //TODO: substituir estas linhas
+        if (!authorization.startsWith("Bearer "))
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid JWT token");
-        String token = authorization.substring("Bearer: ".length());
+        String token = authorization.substring("Bearer ".length());
         if (!userService.validateToken(token)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid JWT token");
         }
